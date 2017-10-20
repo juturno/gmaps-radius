4edf9395f49b23f22d379b927d9e4381e4f8c96c
juturno <22642711+juturno@users.noreply.github.com>
2017-10-20 16:46:14 -0400

    Update app.js

--- static/build/js/app.js
+++ static/build/js/app.js
@@ -72,7 +72,7 @@ l=h.substring(0,l.length)!==l?g(""):new g(h.substring(l.length)),l._parentURI=th
     map = new google.maps.Map($('#map')[0], {
       zoom: 15,
       center: new google.maps.LatLng(40.718433, -74.037597),
-      mapType: google.maps.MapTypeId.ROADMAP,
+      mapType: google.maps.MapTypeId.satellite,
       disableDefaultUI: false,
       mapTypeControl: true,
       zoomControl: true,
