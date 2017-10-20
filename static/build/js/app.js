3aaa0e89529416262e38086da397d69bb031e3ba
juturno <22642711+juturno@users.noreply.github.com>
2017-10-20 15:46:18 -0400

    Update app.js

--- static/build/js/app.js
+++ static/build/js/app.js
@@ -191,7 +191,7 @@ l=h.substring(0,l.length)!==l?g(""):new g(h.substring(l.length)),l._parentURI=th
 		coords[markerLabel] = e.latLng;
 		
 		var infowindow = new google.maps.InfoWindow({
-          	  content: marker.position.toString();
+          	  content: marker.position.toString()
         	});
 		
 		google.maps.event.addListener(marker, 'rightclick', function(){
